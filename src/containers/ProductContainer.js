197b742769133e14c33238e3ae661cdbec2b9864
MacBook Pro <Macbookpro@MacBooks-MacBook-Pro-5.local>
2020-08-27 03:21:36 +0700

    proptypes products

--- src/containers/ProductContainer.js
+++ src/containers/ProductContainer.js
@@ -2,6 +2,8 @@ import React,{Component} from 'react';
 import {connect} from 'react-redux'
 import Products from '../components/Products';
 import Product from '../components/Product';
+import PropTypes from 'prop-types'
+
 class ProductContainer extends Component {
     render() {
         var {product}  = this.props;
@@ -22,6 +24,20 @@ class ProductContainer extends Component {
             }
         return result;
     }
+}
+
+ProductContainer.propTypes = {
+    product : PropTypes.arrayOf(
+        PropTypes.shape({
+            id:PropTypes.number.isRequired,
+            name:PropTypes.string.isRequired,
+            image:PropTypes.string.isRequired,
+            description:PropTypes.string.isRequired,
+            price:PropTypes.number.isRequired,
+            inventory:PropTypes.number.isRequired,
+            rating:PropTypes.number.isRequired
+        })
+    ).isRequired
 }
 
 const mapStateToProps = state =>{
